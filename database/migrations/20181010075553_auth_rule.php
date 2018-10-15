a77ff68b09c7d5c8743863545ad248e9770dc718
巴掌 <w86112@163.com>
2018-10-15 16:55:57 +0800

    修改auth_rule数据表

--- database/migrations/20181010075553_auth_rule.php
+++ database/migrations/20181010075553_auth_rule.php
@@ -35,9 +35,12 @@ class AuthRule extends Migrator
         $table
             ->addColumn('name','string',['limit'=>80,'default'=>'','null'=>false,'comment'=>'规则唯一标识'])
             ->addColumn('title','string',['limit'=>80,'default'=>'','null'=>false,'comment'=>'规则中文名'])
-            ->addColumn('type','boolean',['default'=>false,'null'=>true,'comment'=>'是否是菜单'])
+            ->addColumn('type','boolean',['default'=>false,'null'=>true,'comment'=>''])
             ->addColumn('status','boolean',['default'=>true,'null'=>false,'comment'=>'规则状态'])
             ->addColumn('condition','string',['limit'=>100,'default'=>'','null'=>true,'comment'=>'规则表达式'])
+            ->addColumn('paixu','integer',['limit'=>11,'default'=>999,'null'=>false,'comment'=>'排序'])
+            ->addColumn('ismenu','boolean',['limit'=>1,'default'=>false,'null'=>false,'comment'=>'是否是菜单'])
+            ->addColumn('pid','integer',['limit'=>3,'default'=>1,'null'=>true,'comment'=>'父ID'])
             ->addColumn('delete_time','integer',['limit'=>11,'null'=>true,'comment'=>'删除时间'])
             ->addIndex(array('name'), array('unique' => true))
             ->create();
