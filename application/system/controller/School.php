e404f69f1a5c88d2a01cdaf5c390a9945c12d596
小松树 <w86112@vip.163.com>
2019-03-29 07:48:38 +0800

    0

--- application/system/controller/School.php
+++ application/system/controller/School.php
@@ -30,61 +30,6 @@ class School extends Base
 
 
     //  获取单位列表数据
-    public function ajaxData1()
-    {
-        // 获取DT的传值
-        $getdt = request()->param();
-
-        //得到排序的方式
-        $order = $getdt['order'][0]['dir'];
-        //得到排序字段的下标
-        $order_column = $getdt['order'][0]['column'];
-        //根据排序字段的下标得到排序字段
-        $order_field = $getdt['columns'][$order_column]['name'];
-        if($order_field=='')
-        {
-            $order_field = $getdt['columns'][$order_column]['data'];
-        }
-        //得到limit参数
-        $limit_start = $getdt['start'];
-        $limit_length = $getdt['length'];
-
-        //得到搜索的关键词
-        $search = [
-            'xingzhi'=>$getdt['xingzhi'],
-            'search'=>$getdt['search']['value'],
-            'order'=>$order,
-            'order_field'=>$order_field
-        ];
-
-
-        // 实例化
-        $sch = new sch;
-
-        // 获取荣誉总数
-        $cnt = $sch->select()->count();
-
-        // 查询数据
-        $data = $sch->search($search);
-        $datacnt = $data->count();
-
-        // 获取当前页数据
-        $data = $data->slice($limit_start,$limit_length);
-
-
-        // 重组返回内容
-        $data = [
-            'draw'=> $getdt["draw"] , // ajax请求次数，作为标识符
-            'recordsTotal'=>$cnt,  // 获取到的结果数(每页显示数量)
-            'recordsFiltered'=>$datacnt,       // 符合条件的总数据量
-            'data'=>$data, //获取到的数据结果
-        ];
-
-        return json($data);
-    }
-
-
-    //  获取单位列表数据
     public function ajaxData()
     {
 
