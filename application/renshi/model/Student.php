22a8f915e34d8727677cf1211fa83893f1629c74
小松树 <w86112@vip.163.com>
2019-06-03 16:24:56 +0800

    修复学生信息状态错误问题

--- application/renshi/model/Student.php
+++ application/renshi/model/Student.php
@@ -77,7 +77,7 @@ class Student extends Base
                         $query->field('id,ruxuenian,paixu')->append(['banjiTitle']);
                     }
                 ])
-                ->field('id,xingming,school,sex,shengri,banji')
+                ->field('id,xingming,school,sex,shengri,banji,status')
                 ->append(['age'])
                 ->select();
 
