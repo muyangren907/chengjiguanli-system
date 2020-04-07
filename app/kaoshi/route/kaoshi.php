1a5435e7f247bd77d6565c9a7d2ca2ed21bfe63d
巴掌 <w86112@vip.163.com>
2020-04-07 15:21:47 +0800

    独立考号模块

--- app/kaoshi/route/kaoshi.php
+++ app/kaoshi/route/kaoshi.php
@@ -12,58 +12,43 @@ use think\facade\Route;
 
 // 考试管理
 Route::group('index', function () {
-	    Route::rule('','Index/index','get');						# 信息列表
-	    Route::rule('data','Index/ajaxdata','post');				# 获取数据
-	    Route::rule('create','Index/create','get');				# 添加信息
-	    Route::rule('save','Index/save','post');					# 保存信息
-	    // Route::rule('read/<id>','Index/read','get');				# 读取信息
-	    Route::rule('edit/<id>','Index/edit','get');				# 修改信息
-	    Route::rule('update/<id>','Index/update','put');			# 更新信息
-	    Route::rule('delete/<id>','Index/delete','delete');		# 删除信息
-	    Route::rule('status','Index/setStatus','post');		# 删除信息
-	    Route::rule('more/<kaoshi_id>','Index/moreAction','get');				# 修改信息
-	    Route::rule('cyschool','Index/cySchool','post');				# 获取参加考试班级
-	    Route::rule('cynianji','Index/cyNianji','post');				# 获取参加考试班级
-	    Route::rule('cybanji','Index/cyBanji','post');				# 获取参加考试班级
-	    Route::rule('cysubject','Index/cySubject','post');				# 获取参加考试学科
-	    Route::rule('kaoshiinfo/<id>','Index/kaoshiInfo','post');				# 获取参加考试的学校、学科、班级、年级等信息
-        Route::rule('luru','Index/luru','post');     # 删除信息
-	});
-
-
-// 考号管理
-Route::group('kaohao', function () {
-	    Route::rule('create/<kaoshi_id>','Kaohao/create','get');						# 信息列表
-	    Route::rule('save','Kaohao/save','post');					# 保存信息
-	    Route::rule('biaoqian/<kaoshi_id>','Kaohao/biaoqian','get');						# 信息列表
-	    Route::rule('biaoqianxls','Kaohao/biaoqianXls','post');						# 信息列表
-	    Route::rule('caiji/<kaoshi_id>','Kaohao/caiji','get');					# 成绩采集下载页面
-	    Route::rule('dwcaiji','Kaohao/dwcaiji','post');						# 成绩采集下载
-	    Route::rule('delete/<kaoshi_id>','Kaohao/delete','delete');						# 成绩采集下载
-	    Route::rule('createall/<kaoshi_id>','Kaohao/createAll','get');						# 信息列表
-	    Route::rule('saveall','Kaohao/saveAll','post');					# 保存信息
-        Route::rule('read/<id>','Kaohao/read','get');             # 读取信息
-	});
+    Route::rule('','Index/index','get');						# 信息列表
+    Route::rule('data','Index/ajaxdata','post');				# 获取数据
+    Route::rule('create','Index/create','get');				# 添加信息
+    Route::rule('save','Index/save','post');					# 保存信息
+    // Route::rule('read/<id>','Index/read','get');				# 读取信息
+    Route::rule('edit/<id>','Index/edit','get');				# 修改信息
+    Route::rule('update/<id>','Index/update','put');			# 更新信息
+    Route::rule('delete/<id>','Index/delete','delete');		# 删除信息
+    Route::rule('status','Index/setStatus','post');		# 删除信息
+    Route::rule('more/<kaoshi_id>','Index/moreAction','get');				# 修改信息
+    Route::rule('cyschool','Index/cySchool','post');				# 获取参加考试班级
+    Route::rule('cynianji','Index/cyNianji','post');				# 获取参加考试班级
+    Route::rule('cybanji','Index/cyBanji','post');				# 获取参加考试班级
+    Route::rule('cysubject','Index/cySubject','post');				# 获取参加考试学科
+    Route::rule('kaoshiinfo/<id>','Index/kaoshiInfo','post');				# 获取参加考试的学校、学科、班级、年级等信息
+    Route::rule('luru','Index/luru','post');     # 删除信息
+});
 
 
 // 考试设置
 Route::group('kaoshiset', function () {
-	    Route::rule('index/<kaoshi_id>','KaoshiSet/index','get');						# 信息列表
-	    Route::rule('data','KaoshiSet/ajaxdata','post');				# 获取数据
-	    Route::rule('create/<kaoshi_id>','KaoshiSet/create','get');		# 添加信息
-	    Route::rule('save','KaoshiSet/save','post');					# 保存信息
-	    Route::rule('edit/<id>','KaoshiSet/edit','get');				# 修改信息
-	    Route::rule('update/<id>','KaoshiSet/update','put');			# 更新信息
-	    Route::rule('delete/<id>','KaoshiSet/delete','delete');		# 删除信息
-	    Route::rule('status','KaoshiSet/setStatus','post');		# 删除信息
-	});
+    Route::rule('index/<kaoshi_id>','KaoshiSet/index','get');						# 信息列表
+    Route::rule('data','KaoshiSet/ajaxdata','post');				# 获取数据
+    Route::rule('create/<kaoshi_id>','KaoshiSet/create','get');		# 添加信息
+    Route::rule('save','KaoshiSet/save','post');					# 保存信息
+    Route::rule('edit/<id>','KaoshiSet/edit','get');				# 修改信息
+    Route::rule('update/<id>','KaoshiSet/update','put');			# 更新信息
+    Route::rule('delete/<id>','KaoshiSet/delete','delete');		# 删除信息
+    Route::rule('status','KaoshiSet/setStatus','post');		# 删除信息
+});
 
 
 // 考试设置
 Route::group('tjlog', function () {
-        Route::rule('index/<kaoshi_id>','TongjiLog/index','get');
-        Route::rule('data','TongjiLog/ajaxData','post');
-    });
+    Route::rule('index/<kaoshi_id>','TongjiLog/index','get');
+    Route::rule('data','TongjiLog/ajaxData','post');
+});
 
 
 
