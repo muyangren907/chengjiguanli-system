583481527b264915e6434005645ca5a57ef90ed4
win10 <w86112@vip.163.com>
2020-09-02 15:22:14 +0800

    修复已知错误

---
+++ app/chengji/common.php
@@ -0,0 +1,107 @@
+<?php
+    /**
+    * 从统计结果中获取条形统计图中需要的各项目成绩
+    *
+    * @access public
+    * @param arr 统计结果
+    * @param xm 要统计的项目
+    * @return array 返回类型
+    */
+    function tiaoxingOnexiangmu($jg, $xm)
+    {
+        $chengji = array();
+        $series = array();
+        foreach ($jg as $key => $value) {
+            if($key ==0)
+            {
+                $chengji[$key][] = '项目';
+                foreach ($value['chengji'] as $k => $val) {
+                    $chengji[$key][] = $val['title'];
+                    $series[] = ['type'=>'bar'];
+                }
+            }
+            # code...
+            if( isset($value['banji_title']))
+            {
+                $temp = $value['school_jiancheng'].$value['banji_title'];
+            }else{
+                $temp = $value['school_jiancheng'];
+            }
+            $chengji[$key+1][] = $temp;
+            foreach ($value['chengji'] as $k => $val) {
+                $chengji[$key+1][] = $val[$xm];
+            }
+        }
+        $data = [
+            'data'=>$chengji,
+            'series'=>$series,
+        ];
+
+        return $data;
+    }
+
+
+    /**
+    * 从统计结果中获取条形箱体图需要的各项目成绩
+    *
+    * @access public
+    * @param arr 统计结果
+    * @param xm 要统计的项目
+    * @return array 返回类型
+    */
+    function xiangti($jg)
+    {
+        $data = array();
+        $axisData = array();
+        $chengji = array();
+        $category = array();
+
+        foreach ($jg as $key => $value) {
+            if ($key ==0) {
+                foreach ($value['chengji'] as $k => $val) {
+                    $category[$k] = $val['title'];
+                }
+            }
+
+            foreach ($value['chengji'] as $k => $val) {
+                $chengji[$k][$key][0] = $val['sifenwei']['min'];
+                $chengji[$k][$key][1] = $val['sifenwei']['q1'];
+                $chengji[$k][$key][2] = $val['sifenwei']['q2'];
+                $chengji[$k][$key][3] = $val['sifenwei']['q3'];
+                $chengji[$k][$key][4] = $val['sifenwei']['max'];
+            }
+
+            if (isset($value['banji_title'])) {
+                $temp = $value['school_jiancheng'] . $value['banji_title'];
+            } else {
+                $temp = $value['school_jiancheng'];
+            }
+
+            $axisData[] = $temp;
+        }
+
+        $data = [
+            'axisData' => $axisData
+            ,'boxData' => $chengji
+            ,'category' => $category
+        ];
+
+        return $data;
+    }
+
+    /**
+    * 把给定的成绩进行排序
+    * @access public
+    * @param array $arr 要计算的一维数组成绩
+    * @return array $result 返回成绩排序结果
+    */
+    function tjOrder($cj,$type = 'desc')
+    {
+        if ($type == 'desc') {
+            arsort($cj);
+        } else {
+            asort($cj);
+        }
+
+        return $cj;
+    }
