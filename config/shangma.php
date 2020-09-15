edc2d1e5bfb53e6da10edd695b1585ca09861720
巴掌 <w86112@vip.163.com>
2020-09-15 20:42:21 +0800

    0

--- config/shangma.php
+++ config/shangma.php
@@ -8,12 +8,12 @@ use think\facade\Env;
 return [
     // 版本号
     'webtitle'         => '码蚁成绩管理系统',  #系统名称
-    'version'         => 'v1.3.5',   # 系统版本
+    'version'         => 'v1.3.6',   # 系统版本
     'weihu'         => false,  # 是否开启维护跳转
     'shijian'		=> '2020-9-2 12:47',  # 开始时间
     'shichang'		=> '1',			# 时长:单位分钟
     'backup'	=>[
-    	'path'     => './data/',//数据库备份路径 
+    	'path'     => './data/',//数据库备份路径
 	    'part'     => 20971520,//数据库备份卷大小
 	    'compress' => 0,//数据库备份文件是否启用压缩 0不压缩 1 压缩
 	    'level'    => 9 //数据库备份文件压缩级别 1普通 4 一般  9最高
