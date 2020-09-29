0f372b35ec4f75dfcaa716314ea514659c008329
win10 <w86112@vip.163.com>
2020-09-29 16:40:23 +0800

    0

---
+++ app/teach/model/KeChengBiao.php
@@ -0,0 +1,15 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\teach\model;
+
+// 引用数据模型基类
+use app\BaseModel;
+
+/**
+ * @mixin \think\Model
+ */
+class KeChengBiao extends BaseModel
+{
+    //
+}
