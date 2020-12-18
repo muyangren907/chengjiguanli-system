98e15deeac90f6219e08f19091e14f61df12207a
小松树 <w86112@vip.163.com>
2020-12-18 12:10:20 +0800

    增加权限

--- app/common.php
+++ app/common.php
@@ -188,3 +188,11 @@
         $check = $md5->check($inputPassword, $serverPassword);
         return $check;
     }
+
+
+    // 文件大小
+    function format_bytes($size, $delimiter = '') {
+        $units = array('B', 'KB', 'MB', 'GB', 'TB', 'PB');
+        for ($i = 0; $size >= 1024 && $i < 5; $i++) $size /= 1024;
+        return round($size, 2) . $delimiter . $units[$i];
+    }
